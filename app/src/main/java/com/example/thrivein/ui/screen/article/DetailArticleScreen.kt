01f58206d4b4536812037ed137b4dccc736035ec
Rafika Wardah <rafika.wardah12@gmail.com>
2023-11-30 15:52:49 +0700

    CPT-90 #comment Implement UI for List Article and Detail Article

--- app/src/main/java/com/example/thrivein/ui/screen/article/DetailArticleScreen.kt
+++ app/src/main/java/com/example/thrivein/ui/screen/article/DetailArticleScreen.kt
@@ -159,7 +159,7 @@ fun DetailArticleScreen(
                             Text(
                                 modifier = Modifier.padding(top = 10.dp, bottom = 40.dp),
                                 text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                                style = MaterialTheme.typography.bodySmall.copy(
+                                style = MaterialTheme.typography.bodyMedium.copy(
                                     fontWeight = FontWeight.Normal
                                 ),
                                 textAlign = TextAlign.Justify,
